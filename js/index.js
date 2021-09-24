96f2ac19ed24cf352fda71acd94448387c32328b
Maria Mussi <mariamussis@outlook.com>
2021-09-24 17:14:09 -0300

    feat: adding another speach on section

--- js/index.js
+++ js/index.js
@@ -1,13 +1,29 @@
-$(".flip-card").on("click", function () {
-  $(this).toggleClass("flipped");
-  $(this).toggleClass("yellow");
-  $(this).toggleClass("blue");
-});
+$( document ).ready(function() {
+  $(".flip-card").on("click", function () {
+    $(this).toggleClass("flipped");
+    $(this).toggleClass("yellow");
+    $(this).toggleClass("blue");
+  });
 
-$(".owl-carousel").owlCarousel({
-  margin: 10,
-  loop: false,
-  nav: false,
-  items: 1,
-  stagePadding: 70,
-});
+  $(".container-cards .owl-carousel").owlCarousel({
+    margin: 10,
+    loop: false,
+    nav: false,
+    items: 1,
+    stagePadding: 70,
+  });
+  
+  $(".owl-carousel.carousel-speach").owlCarousel({
+    loop: false,
+    nav: false,
+    items: 1,
+    dots: true,
+  });
+
+  // function toggleSpeach() {
+  //   $(".speach-board").toggleClass("hide");
+  //   window.setTimeout(toggleSpeach, 20000);
+  // }
+
+  // toggleSpeach()
+})
